b7e6f77bd715f4afee36f2b8c2ff1c790a44f907
Valdery Alves <valdery.jur@gmail.com>
2023-04-21 12:41:56 -0300

    feat: configures material-ui
    Signed-off-by: Valdery Alves <valdery.jur@gmail.com>

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,8 +1,4 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
 
 export default function Document() {
   return (
